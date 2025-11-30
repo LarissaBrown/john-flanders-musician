7a785f107393d617bcb97c34018558b4f1465ca6
Larissa Brown <larissa@example.com>
2025-11-30 13:57:27 -0700

    Position saxophone 10px above container with full opacity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,14 +26,14 @@ export default function Hero() {
       {/* Content - Centered and balanced, pushed down with less bottom padding */}
       <div className="relative z-20 w-full max-w-6xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 text-center py-8 sm:py-10 md:py-12 lg:py-16 mt-32 sm:mt-36 md:mt-40 lg:mt-48">
 
-        {/* Saxophone Illustration - Absolute position to scroll with page, above container */}
-        <div className="absolute -top-72 sm:-top-80 md:-top-88 lg:-top-96 left-1/2 transform -translate-x-1/2 z-30 w-16 h-20 sm:w-20 sm:h-24 md:w-24 md:h-28 lg:w-28 lg:h-32" style={{rotate: '15deg', opacity: '0.7'}}>
+        {/* Saxophone Illustration - 10px above hero container, no transparency */}
+        <div className="absolute -top-24 sm:-top-28 md:-top-32 lg:-top-36 left-1/2 transform -translate-x-1/2 z-30 w-16 h-20 sm:w-20 sm:h-24 md:w-24 md:h-28 lg:w-28 lg:h-32" style={{rotate: '15deg', opacity: '1'}}>
           <Image
             src="/images/saxophone.png"
             alt="Saxophone illustration"
             fill
             className="object-contain"
-            style={{objectPosition: 'center', scale: '1.5', marginTop: '200px'}}
+            style={{objectPosition: 'center', scale: '1.5'}}
             sizes="(max-width: 640px) 64px, (max-width: 768px) 80px, (max-width: 1024px) 96px, 112px"
             unoptimized
           />
